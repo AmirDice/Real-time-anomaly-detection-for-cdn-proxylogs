3a6aeda1b0b80d55039e6e1b2351e69e57c7b90f
Amir <inuwaamir@gmail.com>
2022-12-05 19:05:25 +0100

    update

--- kafka/newcons.py
+++ kafka/newcons.py
@@ -4,11 +4,11 @@ from kafka import KafkaConsumer
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 import time
+from datetime import datetime
 
-if __name__ == '__main__':
     # Kafka Consumer 
-    topic_name = "CDN"
-    consumer = KafkaConsumer(
+topic_name = "cdn"
+consumer = KafkaConsumer(
         topic_name,
         bootstrap_servers=['localhost:9092'],
         auto_offset_reset='earliest',
@@ -16,13 +16,35 @@ if __name__ == '__main__':
         group_id='cdn-group'
 )
     # database visualization connection info
-    token = "CC8zymsGh-0xRp_oMlLV2XL24sGF7mIRXugC9Hfu9FlariNPUmz7AnC_1vU-TUjPrE0RMwgodMMsyPdoJRlrFg=="
-    org = "primary"
-    bucket = "cdn"
+token = "hX641N2prnezgaopCHbeXFRQHMIL7OXcisZBYB5Vb4CbZsQSHz5OLSZiR87Wo8DhHApQ1kmNOkReq88FW-6e4w=="
+org = "cdn"
+bucket = "cdn"
     
-    for message in consumer:
-        #connect to influx db client
-        client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
+for message in consumer:
+        mvalue = json.loads(message.value.decode('utf-8'))
+        mkey = message.key
+        mpart = message.partition
+        moffset = message.offset
+
+        client = InfluxDBClient(url="http://localhost:8086", username="admin", password="password123", token=token, org=org)
         write_api = client.write_api(write_options=SYNCHRONOUS)
-        print(json.loads(message.value))
-        print('Writing was successful to the Kafka topic');
+
+        print(message)
+        
+        dataPoint = Point("cdnproxyData")\
+        .field('protocol', float(mvalue['protocol'])) \
+        .field('contentlength', float(mvalue['contentlength'])) \
+        .field('timefirstbyte', float(mvalue['timefirstbyte'])) \
+        .field('osfamily', float(mvalue['osfamily'])) \
+        .field('uamajor', float(mvalue['uamajor'])) \
+        .field('path', float(mvalue['path'])) \
+        .field('devicebrand', float(mvalue['devicebrand'])) \
+        .field('method', float(mvalue['method'])) \
+        .time(datetime.strptime(mvalue['timestamp'], '%Y-%m-%d %H:%M:%S') )
+
+
+        print(dataPoint)
+
+        write_api.write(bucket=bucket, record=dataPoint)
+        
+        print('Writing was successful to Kafka topic');
